6d1d6485c43e6fe72e8bc654629a63c7db98138f
Fatumayattani <fyattani@gmail.com>
2023-05-08 07:27:45 +0000

    save changes

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -1,84 +1,47 @@
-/* Script that listen for changes on each INPUT checkbox tag */
-/* Dynamic Funcionality */
-$('document').ready(function () {
-  /* Listens for changes on each INPUT checkbox tag */
-  const amenitiesId = {};
-  $('INPUT[type="checkbox"]').click(function () {
-    if ($(this).prop('checked')) {
-      amenitiesId[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenitiesId[$(this).attr('data-id')];
-    }
-    $('.amenities h4').text(Object.values(amenitiesId).join(', '));
-  });
-  /* Get status of api and change class if api not available
-  Request status every 10 seconds */
-  /* Simple way, manual re-loading page: */
-  /* $.get(`http://${window.location.hostname}:5001/api/v1/status/`, function(status){
-    if (status.status === 'OK') {
-      $('DIV#api_status').addClass('available');
-    } else {
-       $('DIV#api_status').removeClass('available');
-    }
-  });
-  */
-  const callout = function () {
-    $.ajax({
-      type: 'get',
-      url: `http://${window.location.hostname}:5001/api/v1/status/`,
-      timeout: 5000,
-      success: function (status) {
-        if (status.status === 'OK') {
-          $('DIV#api_status').addClass('available');
-        } else {
-          $('DIV#api_status').removeClass('available');
-        }
-      },
-      error: function () {
-        $('DIV#api_status').removeClass('available');
-      },
-      complete: function () {
-        setTimeout(callout, 10000);
-      }
-    });
-  };
-  callout();
-  /*
-    Retrieve all places and create a articule tag with them
-  */
-  const getPlaces = function () {
-    $.ajax({
-      type: 'POST',
-      contentType: 'application/json',
-      url: 'http://localhost:5001/api/v1/places_search/',
-      data: '{}',
-      dataType: 'json',
-      success: function (places) {
-        $.each(places, function (index, place) {
-          $('.places').append(
-            '<article>' +
-              '<div class="title_box">' +
-              '<h2>' + place.name + '</h2>' +
-              '<div class="price_by_night">' + place.price_by_night +
-              '</div>' +
-              '</div>' +
-              '<div class="information">' +
-              '<div class="max_guest">' +
-              '<br />' + place.max_guest + ' Guests' +
-              '</div>' +
-              '<div class="number_rooms">' +
-              '<br />' + place.number_rooms + ' Bedrooms' +
-              '</div>' +
-              '<div class="number_bathrooms">' +
-              '<br />' + place.number_bathrooms + ' Bathroom' +
-              '</div>' +
-              '</div>' +
-              '<div class="description">' + place.description +
-              '</div>' +
-              '</article>');
-        });
-      }
-    });
-  };
-  getPlaces();
-});
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+import uuid
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/3-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('1-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+			   cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
